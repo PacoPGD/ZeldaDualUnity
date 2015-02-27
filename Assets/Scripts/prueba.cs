ba0b6cb9d71a577af2e9511a503b6c2a93d11426
Francisco García Díaz <pakiyo_pgd@hotmail.com>
2015-02-27 16:59:52 +0100

    Fondos e imagenes configuradas

---
+++ Assets/Scripts/prueba.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class prueba : MonoBehaviour 
+{
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+	
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rigidbody.velocity = movement * speed;
+		
+		rigidbody.position = new Vector2
+			(
+				Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax),
+				Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
+				);
+		
+	}
+	
+	
+}
