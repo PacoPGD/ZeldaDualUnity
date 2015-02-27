e6e581c9a52a61eb1ee45efb4a2bbb350cd419ab
Francisco García Díaz <pakiyo_pgd@hotmail.com>
2015-02-27 17:19:45 +0100

    Movimiento de personaje

--- Assets/Scripts/movimientoJugador.cs
+++ Assets/Scripts/movimientoJugador.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class movimientoJugador : MonoBehaviour {
 
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () 
+	{
+		movimiento ();
+	}
 
+	void movimiento()
+	{
+		float movimientoHorizontal = Input.GetAxis ("Horizontal");
+		float movimientoVertical = Input.GetAxis ("Vertical");
+
+		if(movimientoHorizontal==1 && movimientoVertical ==1)
+			rigidbody2D.velocity = new Vector2 (movimientoHorizontal * 7.07, movimientoVertical * 7.07);
+		else
+			rigidbody2D.velocity = new Vector2 (movimientoHorizontal * 10, movimientoVertical * 10);
 	}
 }
