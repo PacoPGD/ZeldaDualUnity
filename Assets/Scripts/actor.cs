79a4593b06f84ca331ba0c0402cb701341305c3d
Francisco García Díaz <pakiyo_pgd@hotmail.com>
2015-02-28 03:10:12 +0100

    Creación de actor y profundidad en capa

---
+++ Assets/Scripts/actor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class actor : MonoBehaviour {
+
+	private Animator anim;
+	private int facing;
+	private bool moving;
+	
+	// Use this for initialization
+	public void Start () {
+		
+		anim = this.GetComponent<Animator>();
+		moving = false;
+		facing = 2;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		animacionMovimiento(movimiento ());
+		transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
+	}
+
+	public abstract float[]  movimiento ();
+
+
+	void animacionMovimiento(float [] move){
+		//codigo para la animacion
+		moving = move[0] != 0 || move[1] != 0;
+		if (moving) {
+			if (move[1] > 0) facing = 8;
+			else if (move[1] < 0)facing = 2;
+			else if (move[0] > 0) facing = 6;
+			else facing = 4;
+		}
+		
+		anim.SetBool("moving",moving);
+		anim.SetInteger ("facing", facing);
+	}
+}
---
+++ Assets/Scripts/jugador.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jugador : actor {
+
+	public int nJugador;
+
+	public override float[]  movimiento (){
+		float [] move = new float[2];
+		
+		
+		if (nJugador == 1) {
+			move[0] = Input.GetAxis ("Horizontal1");
+			move[1] = Input.GetAxis ("Vertical1");
+		} 
+		else {
+			move[0] = Input.GetAxis ("Horizontal2");
+			move[1] = Input.GetAxis ("Vertical2");
+		}
+		
+		float velocidad = 10;
+		
+		if (move[0] != 0 && move[1] != 0)
+			velocidad = Mathf.Sqrt(Mathf.Pow(velocidad,2)*2)/2;
+		
+		rigidbody2D.velocity = new Vector2 (move[0] * velocidad, move[1] * velocidad);
+		
+		return move;
+	}
+
+
+
+}
