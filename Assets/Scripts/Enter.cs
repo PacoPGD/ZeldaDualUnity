a0d4bdd34ab69875665e8163703b488035695f64
Francisco García Díaz <pakiyo_pgd@hotmail.com>
2015-02-28 17:07:30 +0100

    Añadiendo menus y contenidos

---
+++ Assets/Scripts/Enter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("A1")!=0){ 
+			Application.LoadLevel ("eligePartida"); 
+		} 
+	}
+}
