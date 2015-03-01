6e91fee5c4a103483933d3d9d689bad98b17004b
Centollo <trillo.francisco24@gmail.com>
2015-03-01 02:07:51 +0100

    Intento de GUI y collider de link

--- Assets/Scripts/Hablador.cs
+++ Assets/Scripts/Hablador.cs
@@ -4,26 +4,39 @@ using System.Collections;
 public class Hablador : MonoBehaviour {
 
 	public string mensaje;
+	private bool hablar;
 
+	void Start () {
+		hablar = false;
+	}
 	
 	void OnTriggerStay2D(Collider2D other){
 		if(other.gameObject.name.Equals("Jugador1")){
 			if(Input.GetAxis ("A1")!=0)
-				imprimeMensaje();
+				//imprimeMensaje();
+				hablar = true;
 		}
 		if(other.gameObject.name.Equals("Jugador2")){
 			if(Input.GetAxis ("A2")!=0)
-				imprimeMensaje();
+				//imprimeMensaje();
+				hablar = true;
 		}
 	}
 
 	void imprimeMensaje(){
 		Debug.Log (mensaje);
 		//GUI.Box(Rect (Screen.width/2-(PosX/2), Screen.height/2-(PosY/2), PosX, PosY),mensaje);
-		OnGUI ();
 	}
 
 	void OnGUI() {
-		GUI.Box(new Rect(5,5,5,5), mensaje); 
+		if (hablar) {
+			Debug.Log (mensaje);
+			//GUILayout.BeginArea(new Rect(0,0,Screen.width,Screen.height));
+			//GUI.Label(new Rect(10, 10, 100, 20), "Hello World!");
+			//GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "This is a title");
+			//GUILayout.Label(mensaje);
+			hablar = false;
+			//GUILayout.EndArea();
+		} 
 	}
 }
